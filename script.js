5969b72be9a016ff95426430d2c3285699f5de9b
leonardauguste <leonardaugustee@gmail.com>
2025-10-03 09:51:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,6 +87,25 @@ function printSchedule() {
   document.body.innerHTML = original;
 }
 
+// tombol scroll ke atas
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
 
 
 
